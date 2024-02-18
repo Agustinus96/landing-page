dc81d7055ba770a976fa5052e12a35dd666cf311
アグス・ティヌス <agusutinus@agusutinusus-MacBook-Pro.local>
2024-02-18 11:26:18 +0900

    auth

--- pages/api/auth/error.js
+++ pages/api/auth/error.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react'
-import Soon from '../components/comingsoon/comingsoon'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
-import Container from '../components/container'
-import SectionTitle from '../components/sectionTitle'
-import Tables from '../components/pricing/table'
-import PopupWidget from '../components/popupWidget'
-import Company from '../components/company/company'
-import { founder } from '../components/company/teams-data'
-import Teams from '../components/company/teams'
+import Navbar from '../../../components/navbar'
+import Footer from '../../../components/footer'
+import SectionTitle from '../../../components/sectionTitle'
+import PopupWidget from '../../../components/popupWidget'
 
 const Pricing = () => {
     return (
