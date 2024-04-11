548be400fd4f2df49b59757b8731240222f617b2
Agustinus96 <agustinus1996@gmail.com>
2024-04-11 20:34:19 +0900

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,25 @@ import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from 'next-i18next';
+import Script from 'next/script';
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session} >
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=YOUR_GA_TRACKING_ID`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-H0NPV552C8');
+        `}
+      </Script>
       <ThemeProvider attribute="class"r>
       <Component {...pageProps} />
       </ThemeProvider>
